f6c13e9f6b4d11b2a5c7fc440c1839c68b548818
jakob-dunning <info@duckdesigns.de>
2013-04-06 18:43:28 +0300

    Update from remote Storage Weekend 6.4. 2013

--- contacts.js
+++ contacts.js
@@ -1,51 +1,94 @@
 remoteStorage.defineModule('contacts', function(privateClient, publicClient) {
-  return {
-    exports: {
-      init: function () {
-        privateClient.release('');
-        publicClient.release('');
-        privateClient.declareType('contact', {
-          description: 'an entry in the user\'s addressbook',
-          type: 'object',
-          properties: {
-            platform: { type: 'string', description: 'Refers to a platform as defined by sockethub, for instance \'facebook\' or \'email\'', required: true },
-            name: { type: 'string', description: 'human-readable full name', required: true },
-            identifier: { type: 'string', description: 'a string that uniquely identifies this user within the platform', required: true }
-          }
-        });
-      },
-
-      getContactsByPlatform: function(platform) {
-        return privateClient.getAll('platform/'+platform+'/');
-      },
-
-      insertSeen: function(userAddress) {
-        var oldSeen = getObject('seen/address/'+userAddress).seen;
-        storeObject('seen/address/'+userAddress).seen, now);
-        storeObject('seen/time/'+Ms+ks+ms, userAddress);
-        removeObject('seen/time/'+MsOld+ksOld+msOld);
-      },
-      
-      getContactsByRecency: function(endTime, maxNo) {//timestamp, previous (rehang). paged?
-        //we could do megasecond/kilosecond/item, and then find and delete the old one.
-        return privateClient.getAll(platform+'/');
-      },
-
-      getContactsByPrefix: function(prefix) {
-        if(prefix.length==3) {
-          return privateClient.getAll('prefix/'+prefix+'/');
-        } else {
-          //...
+  
+  // declaring data type "contact"
+  privateClient.declareType('contact', 'http://json-schema.org/card', {
+    "$schema": "http://json-schema.org/draft-03/schema#",
+    "description": "A representation of a person, company, organization, or place",
+    "type": "object",
+    "properties": {
+        "fn": {
+            "description": "Formatted Name",
+            "type": "string"
+        },
+        "familyName": { "type": "string", "required": true },
+        "givenName": { "type": "string", "required": true },
+        "additionalName": { "type": "array", "items": { "type": "string" } },
+        "honorificPrefix": { "type": "array", "items": { "type": "string" } },
+        "honorificSuffix": { "type": "array", "items": { "type": "string" } },
+        "nickname": { "type": "string" },
+        "url": { "type": "string", "format": "uri" },
+        "email": {
+            "type": "object",
+            "properties": {
+                "type": { "type": "string" },
+                "value": { "type": "string", "format": "email" }
+            }
+        },
+        "tel": {
+            "type": "object",
+            "properties": {
+                "type": { "type": "string" },
+                "value": { "type": "string", "format": "phone" }
+            }
+        },
+        "adr": { "$ref": "http: //json-schema.org/address" },
+        "geo": { "$ref": "http: //json-schema.org/geo" },
+        "tz": { "type": "string" },
+        "photo": { "type": "string" },
+        "logo": { "type": "string" },
+        "sound": { "type": "string" },
+        "bday": { "type": "string", "format": "date" },
+        "title": { "type": "string" },
+        "role": { "type": "string" },
+        "org": {
+            "type": "object",
+            "properties": {
+                "organizationName": { "type": "string" },
+                "organizationUnit": { "type": "string" }
+            }
         }
-      },
-
-      addContact: function(platform, identifier, name) {
-        return privateClient.storeObject('contact', platform+'/'+identifier, {
-          platform: platform,
-          identifier: identifier,
-          name: name
-        });
+    }
+  });
+  return {
+    // function to return the right directory for indices
+    function getIndexDirectory(str)
+    {
+      var words = str.split(' ');
+      for(var i=0;i<words.length;i++) {
+        if(words[i].length>=3) {
+          privateClient.storeObject('link', 'prefix/'+str.substring(0, 3)+'/'+uuid, {
+            links: [
+              {
+                type: 'full',
+              }
+            ]
+          });
+        }
       }
+    }
+    exports: {
+      setContact: function (contact) {
+        // validate before indexing
+        var validationErrors = validateObject(contact, 'http://json-schema.org/card');
+        if(!validationErrors)
+        {
+          // faily- + given + -nickname + email + uuid -> Index
+          
+          
+          privateClient.storeObject("contact", "card/" + privateClient.uuid(), contact);
+        }
+        else
+        {
+          console.log("Validation failed: ", validationErrors);
+        }
+        return privateClient.storeObject("contact", "card/" + privateClient.uuid(), contact);
+      },
+      getContact: function (uuid) {
+        return privateClient.getObject("card/" + uuid);
+      }
+      searchContact: function (string, directory) {
+        // search in directory w. string
+      },
     }
   };
 });
