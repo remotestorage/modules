1a5505bc6539d90961e30205c9302e77cec0ec43
Nick Jennings <nick@silverbucket.net>
2013-09-09 20:37:43 +0200

    very basic sockethub config module

---
+++ src/sockethub.js
@@ -0,0 +1,52 @@
+(function () {
+  var moduleName = 'sockethub';
+
+  RemoteStorage.defineModule(moduleName, function(privateClient, publicClient) {
+    privateClient.declareType('config', {
+      "description" : "sockethub config file",
+      "type" : "object",
+      "properties": {
+        "host": {
+          "type": "string",
+          "description": "the hostname to connect to",
+          "format": "uri",
+          "required": true
+        },
+        "port": {
+          "type": "number",
+          "description": "the port number to connect to",
+          "required": true
+        },
+        "path": {
+          "type": "string",
+          "description": "path portion of the URI, if any",
+          "required": false
+        },
+        "tls": {
+          "type": "boolean",
+          "description": "whether or not to use TLS",
+          "required": false
+        },
+        "secret": {
+          "type": "string",
+          "description": "the secret to identify yourself with the sockethub server",
+          "required": true
+        }
+      }
+    });
+    return {
+      exports: {
+        c: privateClient,
+
+        getConfig: function () {
+          return privateClient.getObject('config.json');
+        },
+
+        writeConfig: function (data) {
+          return privateClient.storeObject('config', 'config.json', data);
+        }
+      }
+    };
+  });
+
+})();
