096898fa3155ee48424298dd6a1ba2cdf8e9d78b
Michiel@unhosted <michiel@unhosted.org>
2014-03-30 09:33:01 +0530

    add three functional modules

---
+++ utils/syncedvar.js
@@ -0,0 +1,27 @@
+function SyncedVar(name, baseClient) {
+  var data, loaded = false;
+  baseClient.on('change', function(e) {
+    if(e.relativePath === name) {
+      data = e.newValue;
+      delete data['@context'];
+    }
+  });
+
+  return {
+    get: function() {
+      if(typeof(data) === 'object') {
+        delete data['@context'];
+      }
+      return data;
+    },
+    set: function(val) {
+      baseClient.storeObject('SyncedVar', name, val);
+      data=val;
+    },
+    load: function() {
+      return baseClient.getObject(name).then(function(res) {
+        data = res.data;
+      });
+    }
+  };
+}
