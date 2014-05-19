623074fcd106fc1ba7fe23f51d72e41a965a4eca
Michiel de Jong <michiel@unhosted.org>
2014-05-19 13:18:03 +0000

    add unit tests

---
+++ test/facebook-suite.js
@@ -0,0 +1,63 @@
+require('./test/dependencies');
+require('./src/facebook');
+define(['require'], function(require) {
+  var suites = [];
+
+  suites.push({
+    desc: 'facebook',
+    setup: function (env, test) {
+      env.config = {
+        actor: {
+          address: 'user@host.com',
+          name: 'User Host'
+        },
+        object: {
+          objectType: 'credentials',
+          token: 'DEADBEEF'
+        }
+      };
+      env.configBad = {
+        host: false,
+        tls: 'uhh'
+      };
+      remoteStorage.caching.enable('/');
+      env.facebook = remoteStorage.facebookCredentials;
+      test.done();
+    },
+    tests: [
+
+      {
+        desc: "set BAD config.json",
+        willFail: true,
+        run: function (env, test) {
+          env.facebook.writeConfig(env.configBad).then(test.done, function () {
+            test.result(false);
+          });
+        }
+      },
+
+      {
+        desc: "set config.json",
+        run: function (env, test) {
+          env.facebook.writeConfig(env.config).then(test.done, function () {
+            test.result(false);
+          });
+        }
+      },
+
+      {
+        desc: "get config.json",
+        run: function (env, test) {
+          env.facebook.getConfig().then(function (d) {
+            test.assert(d, env.config);
+          }, function () {
+            test.result(false);
+          });
+        }
+      }
+    ]
+
+  });
+
+  return suites;
+});
