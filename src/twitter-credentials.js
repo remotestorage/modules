765d4e6a8d362b6a8ddaea0a776aba307b99416b
Michiel de Jong <michiel@unhosted.org>
2014-05-19 13:25:21 +0000

    add schema

--- src/twitter-credentials.js
+++ src/twitter-credentials.js
@@ -2,6 +2,31 @@ RemoteStorage.defineModule('twitter-credentials', function(privClient, pubClient
   if(!CredentialsStore) {
     throw new Error('please include utils/credentialsstore.js');
   }
+  privClient.declareType('config', {
+    type: 'object',
+    properties: {
+      actor: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          address: { type: 'string' },
+        },
+        required: ['name', 'address']
+      },
+      object: {
+        type: 'object',
+        properties: {
+          objectType: {type: 'string', 'enum': ['credentials'] },
+          access_token :        { type: 'string' },
+          access_token_secret : { type: 'string' },
+          consumer_secret :     { type: 'string' },
+          consumer_key :        { type: 'string' }
+        },
+        required: ['objectType', 'access_token', 'access_token_secret', 'consumer_secret', 'consumer_key']
+      }
+    },
+    required: ['actor', 'object']
+  });
   return {
     exports: CredentialsStore('twitter', privClient)
   };
