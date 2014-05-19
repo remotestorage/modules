3d127d0480990dbe7e2a503479851da8bd805b76
Michiel de Jong <michiel@unhosted.org>
2014-05-19 13:16:37 +0000

    add schema

--- src/facebook-credentials.js
+++ src/facebook-credentials.js
@@ -2,6 +2,28 @@ RemoteStorage.defineModule('facebook-credentials', function(privClient, pubClien
   if(!CredentialsStore) {
     throw new Error('please include utils/credentialsstore.js');
   }
+  privClient.declareType('config', {
+    type: 'object',
+    properties: {
+      actor: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          address: { type: 'string' },
+        },
+        required: ['name', 'address']
+      },
+      object: {
+        type: 'object',
+        properties: {
+          objectType: {type: 'string', 'enum': ['credentials'] },
+          token: { type: 'string' }
+        },
+        required: ['objectType', 'token']
+      }
+    },
+    required: ['actor', 'object']
+  });
   return {
     exports: CredentialsStore('facebook', privClient)
   };
