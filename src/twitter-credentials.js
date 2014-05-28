1a9c1a58805fe1a48a83389721772dfeb38013b4
Michiel de Jong <michiel@unhosted.org>
2014-05-28 18:13:58 +0000

    consumer key is now called API key in Twitter's dev panel

--- src/twitter-credentials.js
+++ src/twitter-credentials.js
@@ -15,10 +15,10 @@ RemoteStorage.defineModule('twitter', function(privClient, pubClient) {
    *   address - not used (must be a string)
    * object - object:
    *   objectType - always 'credentials'
+   *   consumer_key - your API key for the Twitter API (string)
+   *   consumer_secret - your API secret for the Twitter API (string)
    *   access_token - your access token for the Twitter API (string)
    *   access_token_secret - your access token secret for the Twitter API (string)
-   *   consumer_secret - your consumer secret for the Twitter API (string)
-   *   consumer_key - your consumer key for the Twitter API (string)
    */
   privClient.declareType('config', {
     type: 'object',
@@ -35,10 +35,10 @@ RemoteStorage.defineModule('twitter', function(privClient, pubClient) {
         type: 'object',
         properties: {
           objectType: {type: 'string', 'enum': ['credentials'] },
+          consumer_key :        { type: 'string' }, // aka API key
+          consumer_secret :     { type: 'string' }, // aka API secret
           access_token :        { type: 'string' },
-          access_token_secret : { type: 'string' },
-          consumer_secret :     { type: 'string' },
-          consumer_key :        { type: 'string' }
+          access_token_secret : { type: 'string' }
         },
         required: ['objectType', 'access_token', 'access_token_secret', 'consumer_secret', 'consumer_key']
       }
