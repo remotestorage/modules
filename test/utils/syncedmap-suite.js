84e1effd730c50ee0ac1d6cb1bda3807dc60ee97
Michiel de Jong <michiel@unhosted.org>
2014-05-28 17:20:41 +0000

    Merge #45: Add SyncedMap (fixes #43)

---
+++ test/utils/syncedmap-suite.js
@@ -0,0 +1,172 @@
+require('./test/dependencies');
+
+requireAndLoad('./src/utils/syncedmap', 'SyncedMap');
+
+define(['require'], function(require) {
+
+  var suites = [];
+
+  suites.push({
+    desc: 'SyncedMap suite',
+    setup: function (env, test) {
+      env.prefixTree = {
+      };
+      env.responses = {};
+      env.handlers = {
+        change: []
+      };
+
+      function mock(obj, functionName) {
+        obj[functionName] = function() {
+          var i, input = [functionName].concat(Array.prototype.slice.call(arguments));
+          if (!env.responses[input]) {
+            console.log('MISSING (or falsy) RESPONSE', input, Object.keys(env.responses));
+          }
+          env.called.push(input);
+          return env.responses[input];
+        };
+      }
+      
+      tmp = global.PrefixTree;
+      global.PrefixTree = function() {
+        this.constructionArgs = arguments;
+        env.prefixTree = this;
+        mock(this, 'storeFile');
+        mock(this, 'remove');
+        mock(this, 'fireInitial');
+      }
+      global.PrefixTree.prototype.on = function(eventName, handler) { env.handlers[eventName].push(handler);}
+      env.syncedMap = new SyncedMap('foo', {
+        scope: function(path) {
+          return 'fake BaseClient scoped to '+path;
+        }
+      });
+      global.PrefixTree = tmp;
+
+      test.done();
+    },
+    tests: [
+
+      {
+        desc: "constructor",
+        run: function (env, test) {
+          test.assertAnd(env.prefixTree.constructionArgs, {'0': 'fake BaseClient scoped to foo/'});
+          test.done();
+        }
+      },
+
+      {
+        desc: "load",
+        run: function (env, test) {
+          env.called = [];
+          env.responses = {};
+          env.responses[ ['fireInitial'] ] = true;
+
+          env.syncedMap.load();
+          setTimeout(function() {
+            test.assertAnd(env.called, [ [ 'fireInitial']]);
+            test.done();
+          }, 10);
+        }
+      },
+
+      {
+        desc: "get, set, get",
+        run: function (env, test) {
+          var storeFilePromise = promising();
+          env.called = [];
+          env.responses = {};
+          env.responses[ ['storeFile', 'application/json', 'some key', JSON.stringify({bla: 17})] ] = storeFilePromise;
+          
+          test.assertAnd(env.syncedMap.get('some key'), undefined);
+          env.syncedMap.set('some key', {bla: 17});
+          test.assertAnd(env.syncedMap.get('some key'), {bla: 17});
+          storeFilePromise.fulfill({});
+          setTimeout(function() {
+            test.assertAnd(env.called, [ [ 'storeFile', 'application/json', 'some key', '{"bla":17}' ] ]);
+            test.done();
+          }, 10);
+        }
+      },
+
+      {
+        desc: "get, remove, get",
+        run: function (env, test) {
+          var storeFilePromise = promising(), removePromise = promising();
+          env.called = [];
+          env.responses = {};
+          env.responses[ ['storeFile', 'application/json', 'some key', JSON.stringify({bla: 17})] ] = storeFilePromise;
+          env.responses[ ['remove', 'some key'] ] = removePromise;
+          
+          env.syncedMap.set('some key', {bla: 17});
+          test.assertAnd(env.syncedMap.get('some key'), {bla: 17});
+          env.syncedMap.remove('some key');
+          test.assertAnd(env.syncedMap.get('some key'), undefined);
+          storeFilePromise.fulfill({});
+          removePromise.fulfill({});
+          setTimeout(function() {
+            test.assertAnd(env.called, [ [ 'storeFile', 'application/json', 'some key', '{"bla":17}' ], ['remove', 'some key']]);
+            test.done();
+          }, 10);
+        }
+      },
+
+      {
+        desc: "getKeys, set, getKeys",
+        run: function (env, test) {
+          var storeFilePromise1 = promising(), storeFilePromise2 = promising(), storeFilePromise3 = promising();
+          env.called = [];
+          env.responses = {};
+          env.responses[ ['storeFile', 'application/json', 'some key', JSON.stringify({bla: 17})] ] = storeFilePromise1;
+          env.responses[ ['storeFile', 'application/json', 'some key', JSON.stringify({bla: 18})] ] = storeFilePromise2;
+          env.responses[ ['storeFile', 'application/json', 'some other key', JSON.stringify({bla: 19})] ] = storeFilePromise3;
+          
+          test.assertAnd(env.syncedMap.getKeys(), []);
+          env.syncedMap.set('some key', {bla: 17});
+          env.syncedMap.set('some key', {bla: 18});
+          env.syncedMap.set('some other key', {bla: 19});
+          test.assertAnd(env.syncedMap.getKeys().length, 2);
+          test.assertAnd(env.syncedMap.getKeys().indexOf('some key') !== -1, true);
+          test.assertAnd(env.syncedMap.getKeys().indexOf('some other key') !== -1, true);
+          storeFilePromise1.fulfill({});
+          storeFilePromise2.fulfill({});
+          storeFilePromise3.fulfill({});
+          setTimeout(function() {
+            test.assertAnd(env.called, [
+              [ 'storeFile', 'application/json', 'some key', '{"bla":17}' ],
+              [ 'storeFile', 'application/json', 'some key', '{"bla":18}' ],
+              [ 'storeFile', 'application/json', 'some other key', '{"bla":19}' ]
+            ]);
+            test.done();
+          }, 10);
+        }
+      },
+
+      {
+        desc: "incoming updates (local from PrefixTree.fireInitial, as well as remote)",
+        run: function (env, test) {
+          env.called = [];
+          env.handlers['change'][0]({
+            origin: 'local',
+            key: 'some key',
+            newValue: JSON.stringify({incoming: 'value'}),
+            newContentType: 'application/json'
+          });
+          env.handlers['change'][0]({
+            origin: 'remote',
+            key: 'some other key',
+            newValue: JSON.stringify({incoming: 'value'}),
+            newContentType: 'application/json'
+          });
+          test.assertAnd(env.syncedMap.get('some key'), {incoming: 'value'});
+          test.assertAnd(env.syncedMap.get('some other key'), {incoming: 'value'});
+          test.assertAnd(env.called, []);
+          test.done();
+        }
+      }
+    ]
+
+  });
+
+  return suites;
+});
