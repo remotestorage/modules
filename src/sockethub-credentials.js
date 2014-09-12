855a039496fd14dd6761f44df5b666b955ba90fd
Nick Jennings <nick@silverbucket.net>
2014-09-12 19:20:21 +0200

    fixing naming and credential formats

--- src/sockethub-credentials.js
+++ src/sockethub-credentials.js
@@ -1,60 +1,46 @@
 /**
- * File: Sockethub
+ * File: Sockethub Credentials
  *
- * Maintainer: Michiel de Jong <michiel@unhosted.org>
- * Version: -    0.1.0
+ * Maintainer: Nick Jennings <nick@silverbucket.net>
+ * Version: -    0.2.0
  *
  */
-(function () {
-  var moduleName = 'sockethub';
-
+RemoteStorage.defineModule('sockethub-credentials', function(privateClient, publicClient) {
   if(!CredentialsStore) {
     throw new Error('please include utils/credentialsstore.js');
   }
 
-  RemoteStorage.defineModule(moduleName, function(privateClient, publicClient) {
-    privateClient.declareType('config', {
-      "description" : "sockethub config file",
-      "type" : "object",
-      "properties": {
-        "host": {
-          "type": "string",
-          "description": "the hostname to connect to",
-          "format": "uri",
-          "required": true
-        },
-        "port": {
-          "type": "number",
-          "description": "the port number to connect to",
-          "required": true
-        },
-        "path": {
-          "type": "string",
-          "description": "path portion of the URI, if any",
-          "required": false
-        },
-        "tls": {
-          "type": "boolean",
-          "description": "whether or not to use TLS",
-          "required": false
-        },
-        "register": {
-          "description" : "sockethub config file",
-          "type" : "object",
-          "properties": {
-            "secret": {
-              "type": "string",
-              "description": "the secret to identify yourself with the sockethub server",
-              "required": true
-            }
-          },
-          "required": true
-        }
+  privateClient.declareType('credentials', {
+    type: 'object',
+    description: 'sockethub credentials file',
+    required: ['host', 'port', 'secret'],
+    additionalProperties: false,
+    properties: {
+      host: {
+        type: 'string',
+        description: 'the hostname to connect to',
+        format: 'uri'
+      },
+      port: {
+        type: 'number',
+        description: 'the port number to connect to'
+      },
+      path: {
+        type: 'string',
+        description: 'path portion of the URI, if any'
+      },
+      tls: {
+        type: 'boolean',
+        description: 'whether or not to use TLS'
+      },
+      secret: {
+        type: 'string',
+        description: 'the secret to identify yourself with the sockethub server'
       }
-    });
-    return {
-      exports: new CredentialsStore('sockethub', privateClient)
-    };
+    }
   });
 
-})();
+  return {
+    exports: new CredentialsStore('sockethub-credentials', privateClient)
+  };
+});
