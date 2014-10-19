7d596f9aa6eca3301bf6710bb33fc56f805ea4b0
Michiel de Jong <michiel@unhosted.org>
2014-10-19 14:18:47 +0100

    update apps module from starter-kit (includes switch to bluebird)

--- src/apps.js
+++ src/apps.js
@@ -7,56 +7,109 @@
  */
 RemoteStorage.defineModule('apps', function(privClient, pubClient) {
   var apps = {},
-    defaultApps = {
-      litewrite: {
-        href: 'http://litewrite.net', // <-- optional; defaults to https://name.5apps.com/ (lowercased)
-        img: '/img/litewrite.png', // <-- optional; defaults to /img/name.png (lowercased)
-        name: 'litewrite' // <-- optional; defaults to name
-      },
-      Laverna: { href: 'https://laverna.cc/' },
-      mcnotes: { },
-      //(no rs) StackEdit: { href: 'https://stackedit.io/' },
-      gHost: { },
-      dogfeed: { },
-      dogtalk: { },
-      // sharesome: { img: 'none' },
-      grouptabs: { },
-      byoDB: { href: 'http://diafygi.github.io/byoDB/examples/diary/' },
-      'time tracker': { href: 'http://shybyte.github.io/unhosted-time-tracker/', img: '/img/time.png' },
-      'svg-edit': { href: 'https://svg-edit.5apps.com/editor/svg-editor.html'},
-      browser: { href: 'https://remotestorage-browser.5apps.com/' },
-      vidmarks: { },
-      music: { href: 'https://music-michiel.5apps.com/' },
-      drinks: { href: 'https://myfavoritedrinks.5apps.com/' },
-      todo: { href: 'https://todomvc.5apps.com/labs/architecture-examples/remotestorage/' },
-      'dspace-client': { href: 'https://dspace-nilclass.5apps.com/', img: '/img/dspace.png' },
-      strut: { href: 'http://tantaman.github.com/Strut/' },
-      //(no rs) TiddlyWiki: { href: 'http://www.tiddlywiki.com/' },
-      //(no rs) Fargo: { href: 'http://fargo.io/' },
-      // Crypton: { href: 'http://crypton.io/', img: 'none' },
-      // Dillinger: { href: 'http://dillinger.io/', img: 'none' },
-      // 'freedom js': { href: 'http://freedomjs.org/', img: 'none' },
-      // 'Peer CDN': { href: 'https://peercdn.com/', img: 'none' },
-      // '+PeerServer': { href: 'http://www.peer-server.com/', img: 'none' },
-      // Mylar: { href: 'http://css.csail.mit.edu/mylar/#Software', img: 'none' },
-      // CryptoSphere: { href: 'http://cryptosphere.org/', img: 'none' },
-      // editor: { href: 'https://editor-michiel.5apps.com/' },
-      // social: { href: 'https://social-michiel.5apps.com/' },
-      // email: { href: 'https://email-michiel.5apps.com/' },
-      // smarkers: { href: 'https://smarker-nilclass.5apps.com/' },
-    };
+    defaultApps = {}, channelUrl, currentChannel;
 
-  var changeHandler = function() {
-    console.log('Please call remoteStorage.apps.onChange(handler)');
+  var channelChangeHandler = function() {
+    console.log('Please call remoteStorage.apps.onChannelChange(handler)');
   };
 
+  var appsChangeHandler = function() {
+    console.log('Please call remoteStorage.apps.onAppsChange(handler)');
+  };
+
+  function fetchManifest(manifestUrl) {
+    var pending = Promise.defer(),
+    xhr = new XMLHttpRequest();
+    xhr.open('GET', manifestUrl, true);
+    //xhr.responseType = 'json';
+    xhr.onload = function() {
+      var obj;
+      try {
+        obj = JSON.parse(xhr.response);
+      } catch (e) {
+        pending.reject('could not parse JSON document from ' + manifestUrl);
+        return;
+      }
+      pending.resolve(obj);
+    };
+    xhr.onerror = function() {
+      pending.reject('could not fetch ' + manifestUrl);
+    };
+    xhr.send();
+    return pending.promise;
+  }
+
   function fillInBlanks(key, obj) {
+    var pending.promise;
+    if (obj.manifest) {
+      return fetchManifest(obj.manifest);
+    }
+    pending = Promise.defer();
     obj.href = obj.href || 'https://'+key.toLowerCase()+'.5apps.com/';
     obj.img = obj.img || '/img/'+key.toLowerCase()+'.png';
     obj.name = obj.name || key;
-    return obj;
+    pending.resolve(obj);
+    return pending.promise;
   }
 
+  function setAppChannel(channelUrl) {
+    return privClient.storeFile('plain/txt', 'channel-url', channelUrl).then(function() {
+      return fetchDefaultApps();
+    });
+  }
+  var time = 0;
+  function fetchDefaultApps() {
+    var thisTime = time++;
+    return privClient.getFile('channel-url').then(function(obj) {
+      var pending = Promise.defer();
+      var channelUrl = obj.data;
+      if (typeof channelUrl !== 'string') {
+        channelUrl = 'https://apps.unhosted.org/defaultApps.json';
+        setAppChannel(channelUrl);
+      }
+      if (currentChannel === channelUrl) {
+        pending.resolve();
+        return;
+      }
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', channelUrl, true);
+      xhr.responseType = 'json';
+      xhr.onerror = function() {
+        pending.reject('error fetching app list');
+      };
+      xhr.onload = function() {
+        var numRunning = 0;
+        if (xhr.response === null) {
+          pending.reject('not json');
+          return;
+        }
+        defaultApps = {};
+        for (var i in xhr.response) {
+          numRunning++;
+          fillInBlanks(i, xhr.response[i]).then((function(bindI) {
+            return function(obj) {
+              defaultApps[bindI] = obj;
+              numRunning--;
+              if (numRunning === 0) {
+                pending.resolve();
+              }
+            };
+          })(i), function() {
+            numRunning--;
+            if (numRunning === 0) {
+              pending.resolve();
+            }
+          });
+        }
+        currentChannel = channelUrl;
+        if (numRunning === 0) {
+          pending.resolve();
+        }
+      };
+      xhr.send();
+      return pending.promise;
+    });
+  }
 
   /**
    * Function: remoteStorage.apps.installApp
@@ -89,11 +142,29 @@ RemoteStorage.defineModule('apps', function(privClient, pubClient) {
   }
 
   /**
-   * Function: remoteStorage.apps.onChange
+   * Function: remoteStorage.apps.onChannelChange
    *
    * Set the change event handler. This will be called, with the
-   * dictionary of installed apps as the only argument, whenever the
-   * list of installed apps changes. Example:
+   * app channel URL as the only argument, on page load and then
+   * whenever it changes. Example:
+   *
+   * remoteStorage.apps.onChannelChange(function(url) {
+   *   myChannelUrlInput.value = url;
+   * });
+   *
+   * Parameters:
+   *   handler - a Function that takes a dictionary of apps as its only argument
+   */
+  function onChannelChange(handler) {
+    channelChangeHandler = handler;
+  }
+
+  /**
+   * Function: remoteStorage.apps.onAppsChange
+   *
+   * Set the change event handler. This will be called, with the
+   * dictionary of installed apps as the only argument, once on page load,
+   * and then whenever the list of installed apps changes. Example:
    *
    * remoteStorage.apps.onChange(function(apps) {
    *   myAppsView.reset();
@@ -106,21 +177,24 @@ RemoteStorage.defineModule('apps', function(privClient, pubClient) {
    * Parameters:
    *   handler - a Function that takes a dictionary of apps as its only argument
    */
-  function onChange(handler) {
-    changeHandler = handler;
+  function onAppsChange(handler) {
+    appsChangeHandler = handler;
   }
 
   function init() {
     RemoteStorage.config.changeEvents.window = true;
     privClient.cache('', 'ALL');
     privClient.on('change', function(evt) {
-      if (evt.newValue) {
-        apps[evt.relativePath] = evt.newValue;
+      if (evt.relativePath === 'channel-url') {
+        channelChangeHandler(evt.newValue);
       } else {
-        delete apps[evt.relativePath];
+        if (evt.newValue) {
+          apps[evt.relativePath] = evt.newValue;
+        } else {
+          delete apps[evt.relativePath];
+        }
       }
-      console.log('calling changeHandler with', apps, evt);
-      changeHandler(apps);
+      appsChangeHandler(apps);
     });
 
     /**
@@ -141,10 +215,46 @@ RemoteStorage.defineModule('apps', function(privClient, pubClient) {
       },
       required: ['name']
     });
+  }
 
-    for (var i in defaultApps) {
-      defaultApps[i] = fillInBlanks(i, defaultApps[i]);
+  function getAsset(appName, assetBase, assetPath, authoringPort) {
+    var pending = Promise.defer();
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', assetBase+assetPath, true);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function() {
+      remoteStorage.www.storeFile(authoringPort, xhr.getResponseHeader('Content-Type'), assetPath, xhr.response).then(function() {
+        pending.resolve();
+      }, function() {
+        pending.reject();
+      });
+    };
+    xhr.onerror = function() {
+      pending.reject();
     }
+    xhr.send();
+    return pending.promise;
+  }
+
+  function cloneApp(name) {
+    var pending = Promise.defer(), numDone = 0, i;
+    if (Array.isArray(apps[name].assets) && apps[name].assets.length >= 1) {
+      return remoteStorage.www.addAuthoringPort().then(function(authoringPort) {
+        for (i=0; i<apps[name].assets.length; i++) {
+          getAsset(name, apps[name].href, apps[name].assets[i], authoringPort).then(function() {
+            numDone++;
+            if (numDone === apps[name].assets.length) {
+              pending.resolve();
+            }
+          }, function() {
+            pending.reject('error retrieving one of the assets');
+          });
+        }
+      });
+    } else {
+      pending.reject('could not determine assets of ' + name);
+    }
+    return pending.promise;
   }
 
   /**
@@ -176,13 +286,15 @@ RemoteStorage.defineModule('apps', function(privClient, pubClient) {
    *              apps/app schema defined above.
    */
   function getAvailableApps() {
-    var i, availableApps = {};
-    for (i in defaultApps) {
-      if (!apps[i]) {
-        availableApps[i] = defaultApps[i];
+    return fetchDefaultApps().then(function() {
+      var i, availableApps = {};
+      for (i in defaultApps) {
+        if (!apps[i]) {
+          availableApps[i] = defaultApps[i];
+        }
       }
-    }
-    return availableApps;
+      return availableApps;
+    });
   }
 
   //...
@@ -190,11 +302,14 @@ RemoteStorage.defineModule('apps', function(privClient, pubClient) {
 
   return {
     exports: {
-      onChange: onChange,
+      onChannelChange: onChannelChange,
+      onAppsChange: onAppsChange,
+      setAppChannel: setAppChannel,
       installApp: installApp,
       uninstallApp: uninstallApp,
       getInstalledApps: getInstalledApps,
-      getAvailableApps: getAvailableApps
+      getAvailableApps: getAvailableApps,
+      cloneApp: cloneApp
     }
   };
 });
