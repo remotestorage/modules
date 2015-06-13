2e33a9f8a6d554310174f162d2bbd42cdbb65d10
Sebastian Kippe <sebastian@kip.pe>
2015-06-13 17:49:48 +0100

    Daily messages schema

--- dist/messages-irc.js
+++ dist/messages-irc.js
@@ -12,24 +12,6 @@
 
 RemoteStorage.defineModule("messages-irc", function (client, publicClient) {
 
-  var extend = RemoteStorage.util.extend;
-
-  //
-  // Message object schema
-  // TODO move to central place, re-use for other messages modules
-  //
-
-  var messageObject = {
-    date: {
-      type: "string",
-      format: "date-time"
-    },
-    user: {
-      type: "string"
-    },
-    text: {
-      type: "string" } };
-
   /**
    * Schema: messages-irc/daily
    *
@@ -59,6 +41,78 @@ RemoteStorage.defineModule("messages-irc", function (client, publicClient) {
    * }
    * (end code)
    */
+
+  privateClient.declareType("daily", {
+    type: "object",
+    properties: {
+      "@context": {
+        type: "string",
+        "default": "https://kosmos.org/ns/v1",
+        "enum": ["https://kosmos.org/ns/v1"]
+      },
+      "@id": {
+        type: "string",
+        "default": "messages/irc/freenode/kosmos/",
+        "enum": ["messages/irc/freenode/kosmos/"] },
+      "@type": {
+        type: "string",
+        "default": "ChatChannel",
+        "enum": ["ChatChannel"] },
+      name: {
+        type: "string"
+      },
+      ircURI: {
+        type: "string",
+        format: "uri"
+      },
+      today: {
+        type: "object",
+        properties: {
+          "@id": {
+            type: "string",
+            pattern: "d{4}/d{2}/d{2}"
+          },
+          "@type": {
+            type: "string",
+            "default": "ChatLog",
+            pattern: "^ChatLog$"
+          },
+          messageType: {
+            type: "string",
+            "default": "InstantMessage",
+            pattern: "^InstantMessage$"
+          },
+          previous: {
+            type: "string",
+            pattern: "d{4}/d{2}/d{2}"
+          },
+          next: {
+            type: "string",
+            pattern: "d{4}/d{2}/d{2}"
+          },
+          messages: {
+            type: "array",
+            uniqueItems: true,
+            items: {
+              type: "object",
+              properties: {
+                date: {
+                  type: "string",
+                  format: "date-time"
+                },
+                user: {
+                  type: "string"
+                },
+                text: {
+                  type: "string" }
+              }
+            }
+          }
+        }
+      }
+    },
+    required: []
+  });
 
   var ircMessages = {};
 
