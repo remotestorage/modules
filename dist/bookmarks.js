e9b378ec1823459289d729df152870f1d9c6ae81
Sebastian Kippe <sebastian@kip.pe>
2015-06-13 14:22:18 +0100

    Add updatedAt to bookmarks schema/docs

--- dist/bookmarks.js
+++ dist/bookmarks.js
@@ -19,7 +19,7 @@ RemoteStorage.defineModule("bookmarks", function (privateClient, publicClient) {
   var extend = RemoteStorage.util.extend;
 
   //
-  // Types
+  // Types/Schemas
   //
 
   var baseProperties = {
@@ -36,6 +36,10 @@ RemoteStorage.defineModule("bookmarks", function (privateClient, publicClient) {
     createdAt: {
       type: "string",
       format: "date-time"
+    },
+    updatedAt: {
+      type: "string",
+      format: "date-time"
     }
   };
 
@@ -48,7 +52,8 @@ RemoteStorage.defineModule("bookmarks", function (privateClient, publicClient) {
    *   id          - A string that uniquely identifies this bookmark (required)
    *   url         - The url of the bookmark (required)
    *   title       - The user-facing string describing the bookmark (required)
-   *   createdAt   - DateTime string of when this bookmark was created
+   *   createdAt   - DateTime string of document creation
+   *   updatedAt   - DateTime string of last update
    *   description - Like title, but more elaborate
    *   tags        - Array of strings; use tags to give your bookmarks a loose grouping into categories
    *   thumbnail   - A base64-encoded screenshot of the bookmarked page
@@ -82,7 +87,8 @@ RemoteStorage.defineModule("bookmarks", function (privateClient, publicClient) {
    *   id          - A string that uniquely identifies this bookmark
    *   url         - The url of the bookmark
    *   title       - The user-facing string describing the bookmark
-   *   createdAt   - DateTime string of when this bookmark was created
+   *   createdAt   - DateTime string of document creation
+   *   updatedAt   - DateTime string of last update
    *   tags        - Array of strings; use tags to give your bookmarks a loose grouping into categories
    */
 
@@ -105,7 +111,8 @@ RemoteStorage.defineModule("bookmarks", function (privateClient, publicClient) {
    *   id          - A string that uniquely identifies this bookmark
    *   url         - The url of the bookmark
    *   title       - The user-facing string describing the bookmark
-   *   createdAt   - DateTime string of when this bookmark was created
+   *   createdAt   - DateTime string of document creation
+   *   updatedAt   - DateTime string of last update
    *   unread      - Boolean, whether the bookmark is unread (default: true, required)
    */
 
--- src/bookmarks.js
+++ src/bookmarks.js
@@ -14,7 +14,7 @@ RemoteStorage.defineModule('bookmarks', function (privateClient, publicClient) {
   var extend = RemoteStorage.util.extend;
 
   //
-  // Types
+  // Types/Schemas
   //
 
   var baseProperties = {
@@ -31,6 +31,10 @@ RemoteStorage.defineModule('bookmarks', function (privateClient, publicClient) {
     "createdAt": {
       "type": "string",
       "format": "date-time"
+    },
+    "updatedAt": {
+      "type": "string",
+      "format": "date-time"
     }
   };
 
@@ -44,7 +48,8 @@ RemoteStorage.defineModule('bookmarks', function (privateClient, publicClient) {
    *   id          - A string that uniquely identifies this bookmark (required)
    *   url         - The url of the bookmark (required)
    *   title       - The user-facing string describing the bookmark (required)
-   *   createdAt   - DateTime string of when this bookmark was created
+   *   createdAt   - DateTime string of document creation
+   *   updatedAt   - DateTime string of last update
    *   description - Like title, but more elaborate
    *   tags        - Array of strings; use tags to give your bookmarks a loose grouping into categories
    *   thumbnail   - A base64-encoded screenshot of the bookmarked page
@@ -78,7 +83,8 @@ RemoteStorage.defineModule('bookmarks', function (privateClient, publicClient) {
    *   id          - A string that uniquely identifies this bookmark
    *   url         - The url of the bookmark
    *   title       - The user-facing string describing the bookmark
-   *   createdAt   - DateTime string of when this bookmark was created
+   *   createdAt   - DateTime string of document creation
+   *   updatedAt   - DateTime string of last update
    *   tags        - Array of strings; use tags to give your bookmarks a loose grouping into categories
    */
 
@@ -101,7 +107,8 @@ RemoteStorage.defineModule('bookmarks', function (privateClient, publicClient) {
    *   id          - A string that uniquely identifies this bookmark
    *   url         - The url of the bookmark
    *   title       - The user-facing string describing the bookmark
-   *   createdAt   - DateTime string of when this bookmark was created
+   *   createdAt   - DateTime string of document creation
+   *   updatedAt   - DateTime string of last update
    *   unread      - Boolean, whether the bookmark is unread (default: true, required)
    */
 
