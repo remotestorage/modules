9ede3a80eb65be88c12182e2ec842fb47561d41a
Sebastian Kippe <sebastian@kip.pe>
2015-06-27 20:36:31 +0100

    Message module WIP

--- src/messages-irc.js
+++ src/messages-irc.js
@@ -7,7 +7,12 @@
  * This module stores IRC messages in daily archive files.
  */
 
-RemoteStorage.defineModule('messages-irc', function (client, publicClient) {
+// TODO only load in node.js
+// if (isNodeJs) {
+RemoteStorage = require("remotestoragejs");
+// }
+
+RemoteStorage.defineModule("messages-irc", function (privateClient, publicClient) {
 
   /**
    * Schema: messages-irc/daily
@@ -39,23 +44,22 @@ RemoteStorage.defineModule('messages-irc', function (client, publicClient) {
    * (end code)
    */
 
-  privateClient.declareType('daily', {
+  const archiveSchema = {
     "type": "object",
     "properties": {
       "@context": {
         "type": "string",
         "default": "https://kosmos.org/ns/v1",
-        "enum": [ "https://kosmos.org/ns/v1" ]
+        "enum": ["https://kosmos.org/ns/v1"]
       },
       "@id": {
         "type": "string",
-        "default": "messages/irc/freenode/kosmos/",
-        "enum": [ "messages/irc/freenode/kosmos/" ],
+        "default": "messages-irc/freenode/kosmos/"
       },
       "@type": {
         "type": "string",
         "default": "ChatChannel",
-        "enum": [ "ChatChannel" ],
+        "enum": ["ChatChannel"]
       },
       "name": {
         "type": "string"
@@ -69,7 +73,7 @@ RemoteStorage.defineModule('messages-irc', function (client, publicClient) {
         "properties": {
           "@id": {
             "type": "string",
-            "pattern": "\d{4}\/\d{2}\/\d{2}"
+            "pattern": "^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$"
           },
           "@type": {
             "type": "string",
@@ -83,11 +87,11 @@ RemoteStorage.defineModule('messages-irc', function (client, publicClient) {
           },
           "previous": {
             "type": "string",
-            "pattern": "\d{4}\/\d{2}\/\d{2}"
+            "pattern": "^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$"
           },
           "next": {
             "type": "string",
-            "pattern": "\d{4}\/\d{2}\/\d{2}"
+            "pattern": "^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$"
           },
           "messages": {
             "type": "array",
@@ -103,8 +107,15 @@ RemoteStorage.defineModule('messages-irc', function (client, publicClient) {
                   "type": "string"
                 },
                 "text": {
-                  "type": "string",
-                }
+                  "type": "string"
+                },
+                "type": "string",
+                "default": "text",
+                "enum": [
+                  "text",
+                  "join",
+                  "leave"
+                ]
               }
             }
           }
@@ -112,13 +123,189 @@ RemoteStorage.defineModule('messages-irc', function (client, publicClient) {
       }
     },
     "required": []
-  });
+  }
 
-  const ircMessages = {
+  privateClient.declareType("daily-archive", "https://kosmos.org/ns/v1", archiveSchema);
+  publicClient.declareType("daily-archive", "https://kosmos.org/ns/v1", archiveSchema);
 
+  /**
+   * Class: DailyArchive
+   *
+   * A daily archive stores IRC messages by day.
+   *
+   * Parameters:
+   *   options - (Object) See class properties below for object properties
+   *   TODO not all props are constructor args
+   */
+  // TODO move to common module for all messages modules
+  var DailyArchive = function DailyArchive(options) {
+    //
+    // Defaults
+    //
+    options.isPublic = options.isPublic || false;
+
+    //
+    // Validate options
+    //
+    if (typeof options !== "object") {
+      throw "options must be an object";
+    }
+    if (typeof options.network !== "object" ||
+        typeof options.network.name !== "string" ||
+        typeof options.network.ircURI !== "string") {
+      throw "network must be an object containing server \"name\" and \"ircURI\"";
+    }
+    if (typeof options.channelName !== "string") {
+      throw "channelName must be a string";
+    }
+    if (!(options.date instanceof Date)) {
+      throw "date must be a date object";
+    }
+    if (typeof options.isPublic !== "boolean") {
+      throw "isPublic must be a boolean value";
+    }
+
+    /**
+     * Property: network
+     *
+     * Contains information about the IRC network
+     *
+     * Properties:
+     *   name - Shortname/id of network (e.g. "freenode")
+     *   ircURI - IRC URI of network (e.g. "irc://irc.freenode.net/")
+     */
+    this.network = options.network;
+
+    /**
+     * Property: channelName
+     *
+     * Name of the IRC channel (e.g. "#kosmos")
+     */
+    this.channelName = options.channelName;
+
+    /**
+     * Property: date
+     *
+     * Date of the archive's content
+     */
+    this.date = options.date;
+
+    /**
+     * Property: isPublic
+     *
+     * `true` for public archives, `false` for private ones
+     */
+    this.isPublic = options.isPublic;
+
+    /**
+     * Property: parsedDate
+     *
+     * Object containing padded year, month and day of date
+     */
+    this.parsedDate = parseDate(this.date);
+
+    /**
+     * Property: dateId
+     *
+     * Date string in the form of YYYY/MM/DD
+     */
+    this.dateId = this.parsedDate.year+'/'+this.parsedDate.month+'/'+this.parsedDate.day
+
+    /**
+     * Property: messages
+     *
+     * An array holding all messages of the day
+     */
+    this.messages = [];
+  };
+
+  DailyArchive.prototype = {
+    /*
+     * Method: addMessage
+     *
+     * Parameters:
+     *   timestamp - Timestamp of the message
+     *   from      - The sender of the message
+     *   text      - The message itself
+     *   type      - Type of message (one of text, join, leave)
+     */
+    addMessage: function addMessage(obj) {
+      console.log("addMessage", obj);
+      var message = {
+        "timestamp": obj.timestamp,
+        "from": obj.from,
+        "text": obj.text || '',
+        "type": obj.type || 'text'
+      }
+
+      this.messages.push(message);
+      this.sync();
+    },
+
+    /*
+     * Method: buildArchiveObject
+     *
+     * Builds the object to be stored in remote storage
+     */
+    buildArchiveObject: function() {
+      var channelNameWithoutHash = this.channelName.replace(/^#/, '');
+
+      return {
+        "@id": "messages-irc/"+this.network.name+"/"+channelNameWithoutHash+"/",
+        "@type": "ChatChannel",
+        "name": this.channelName,
+        "ircURI": this.network.ircURI+"/"+this.channelName,
+        "today": {
+          "@id": this.dateId,
+          "@type": "ChatLog",
+          "messageType": "InstantMessage",
+          // "previous": null,
+          // "next": null,
+          "messages": this.messages
+        }
+      };
+    },
+
+    /*
+     * Method: sync
+     *
+     * Find or create archive file, update it, and save it to the storage
+     */
+    sync: function() {
+      var client = this.isPublic ? publicClient : privateClient;
+      var path = this.network.name+"/"+this.channelName.replace(/^#/,'')+"/"+this.dateId;
+
+      var obj = this.buildArchiveObject();
+      console.log('Writing archive object', obj);
+
+      client.storeObject('daily-archive', path, obj).then(function(){
+        console.log('Archive written to remote storage');
+      },function(error){
+        console.log('Error trying to store object', error);
+      });
+    }
+  };
+
+  var parseDate = function(date) {
+    var pad = function(num) {
+      num = String(num);
+      if (num.length === 1) { num = "0" + num; }
+      return num;
+    };
+
+    return {
+      year:  date.getUTCFullYear(),
+      month: pad( date.getUTCMonth() + 1 ),
+      day:   pad( date.getUTCDate() )
+    };
+  }
+
+  var exports = {
+    DailyArchive: DailyArchive,
+    privateClient: privateClient,
+    publicClient: publicClient
   };
 
   // Return public functions
-  return { exports: ircMessages };
-
+  return { exports: exports };
 });
